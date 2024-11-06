e218b7cb938006a71764b62960233b8bcccd8169
Fabiola Pasini <fabiola.pasini@hotmail.it>
2024-11-06 02:20:53 +0100

    feature/ add a logger - just simple usage

--- MapperReducer/mapperReducer/main.cpp
+++ MapperReducer/mapperReducer/main.cpp
@@ -1,14 +1,23 @@
-#include <nlohmann/json.hpp>
+#include <log4cxx/asyncappender.h>
+#include <log4cxx/basicconfigurator.h>
+#include <log4cxx/consoleappender.h>
+#include <log4cxx/helpers/pool.h>
+#include <log4cxx/logmanager.h>
+#include <log4cxx/patternlayout.h>
+#include <log4cxx/propertyconfigurator.h>
+// #include <log4cxx/rollingfileappender.h>
+
 #include <fstream>
 #include <iostream>
+#include <nlohmann/json.hpp>
 #include <string>
 
-#include "src/mapper_reducer.cpp"
 #include "include/utils.h"
+#include "src/mapper_reducer.cpp"
 
 using json = nlohmann::json;
-
-
+using namespace log4cxx;
+using namespace log4cxx::helpers;
 
 // TODO
 /*
@@ -20,8 +29,16 @@ aggiugnere logging e mettere tutti i print li dentro
 aggiungere precommit, si posso fare le pipeline tipo con bamboo?
 */
 
-
 int main() {
+  log4cxx::BasicConfigurator::configure();
+  LoggerPtr logger(Logger::getLogger("MyTestLogger"));
+
+  LOG4CXX_INFO(logger, "Questo è un messaggio informativo");
+  LOG4CXX_DEBUG(logger, "Questo è un messaggio di debug");
+  LOG4CXX_WARN(logger, "Questo è un messaggio di warning");
+  LOG4CXX_ERROR(logger, "Questo è un messaggio di errore");
+  LOG4CXX_FATAL(logger, "Questo è un messaggio fatale");
+
   std::filesystem::path config_json_file =
       std::filesystem::current_path() / "Assets" / "config.json";
   std::ifstream file(config_json_file);
@@ -52,9 +69,8 @@ int main() {
             << std::endl;
   std::cout << "OutputFiles: " << config.foldersInfo.OutputFiles << std::endl;
   */
-  
+
   runProgram(config);
 
   return 0;
 }
-
